029fff872e7ba8bca566d7d605c14895e865e85d
soye <songnan.ye@nokia.com>
2017-02-07 14:34:09 +0800

    import urls as rango_urls instead of 'rango.urls'

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rango import urls as rango_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^rango/', include('rango.urls')),
+	#url(r'^rango/', include('rango.urls')),
+    url(r'^rango/', include(rango_urls)),
 ]
