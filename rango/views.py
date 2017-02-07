7fb35e77dc4cf416da7e3a38d23cb6b69b6150f6
soye <songnan.ye@nokia.com>
2017-02-07 11:23:22 +0800

    resolve URL by reverse

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 # Create your views here.
-from django.http import HttpResponse
-
 def index(request):
-	return HttpResponse('Rango says: Hello world! <br/> <a href="/rango/about">About</a>')
-	
+	#return HttpResponse('Rango says: Hello world! <br/> <a href="/rango/about">About</a>')
+	return HttpResponse("Rango says: Hello world! <br/> <a href='%s'>About</a>" %reverse('about'))
+
 def about(request):
-	return HttpResponse('Rango says here is the about page. <br/> <a href="/rango/">Index</a>.')
+	#return HttpResponse('Rango says here is the about page. <br/> <a href="/rango/">Index</a>')
+	return HttpResponse("<a href='%s'>Index</a>" %reverse('index'))
